989420e9dccfabcf7d1bcd74d8ef51665a60d2fa
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-05-10 21:40:33 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -1,5 +1,6 @@
 import unittest
 import statistics
+import math
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
@@ -11,6 +12,9 @@ class StatsTest(unittest.TestCase):
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
@@ -25,5 +29,22 @@ class StatsTest(unittest.TestCase):
     self.assertTrue(emailAlert.emailSent)
     self.assertTrue(ledAlert.ledGlows)
 
+class EmailAlert():
+    emailSent = False
+
+class LEDAlert():
+    ledGlows = False
+
+class StatsAlerter():
+  def __init__(self, maxThreshold, alerts):
+    self.maxThreshold = maxThreshold
+    self.alerts = alerts
+
+  def checkAndAlert(self, values):
+    computedStats = statistics.calculateStats(values)
+    if computedStats["max"] > self.maxThreshold:
+      self.alerts[0].emailSent = True
+      self.alerts[1].ledGlows = True
+      
 if __name__ == "__main__":
   unittest.main()
